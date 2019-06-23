46b9a926cb7c8fcc95b22e978e61d3831c32f9a7
Andreu Huguet <andreuhuguet@gmail.com>
2019-06-23 23:41:27 +0200

    SVM

--- analysis/rbf.r
+++ analysis/rbf.r
@@ -6,72 +6,49 @@ no.show <- dd.full[, 4]
 dd.num <- dd.full[, -c(2, 4, 12)] # Patient ID, No.show nor Training
 dd.num$DateDiff <- NULL
 
-summary(dd.num)
 dd.num.s <- scale(dd.num)
-summary(dd.num.s)
 
-N <- nrow(dd.num)
-d <- ncol(dd.num)
+N <- nrow(dd.num.s)
+d <- ncol(dd.num.s)
 
-library(MASS)
-library(cclust)
+library(neural)
+bin.show <- as.numeric(no.show) - 1
 
-# Function to compute a PHI (N x M) design matrix, without the Phi_0(x) = 1 column;
-# c.i, sg.i are the centers and smoothing parameters or variances (sigma_i^2) of the neurons, respectively
+dd.training <- as.matrix(dd.num.s[dd.full$Training == 1, ])
+dd.test <- as.matrix(dd.num.s[dd.full$Training == 0, ])
 
-sq.norm <- function(x,y) sum((x - y)^2)
+show.training <- as.matrix(bin.show[dd.full$Training == 1])
+show.test <- as.matrix(bin.show[dd.full$Training == 0])
 
-PHI <- function (x,c,sg) {
-  N <- length(x)
-  M <- length(c)
-  phis <- matrix(rep(0,M*N), nrow=M)
-  for (i in 1:M)
-    phis[i,] <- exp(-(x - c[i])^2/(2*sg[i]))
-  
-  t(phis)
-}
+M <- round(sqrt(N))
 
-## We find the centers and variances for each neuron using k-means;
-## since this clustering algorithm is non-deterministic (because the initial centers are random),
-## we do it 'NumKmeans' times
-NumKmeans <- 2
+# For comparison, we use the same number of centers (M)
+data <- rbftrain (dd.training,
+                  M,
+                  show.training,
+                  it = 40,
+                  visual = FALSE)
 
-## We set a rather large number of hidden units (= basis functions) M as a function of 
-## data size (the sqrt is just a heuristic!) because we are going to try different regularizers
-(M <- floor(sqrt(N)))
 
-# to store the centers and variances
-C <- M
-expxc <- matrix(0, nrow=NumKmeans, ncol=C)
-sg <- matrix(0, nrow=NumKmeans, ncol=C)
+# And make it predict the same test data
+preds <- rbf (dd.test,
+              data$weight,
+              data$dist,
+              data$neurons,
+              data$sigma)
 
-for (j in 1:NumKmeans) {
-  # Find the centers c.i with k-means
-  km.res <- cclust (x = dd.num.s,  # the data is passed as row/column dataframe
-                    centers = C,   # the number of centers is heuristic
-                    iter.max = 200,
-                    method = "kmeans",
-                    dist = "euclidean")
-  
-  c <- km.res$centers
-  interval <- c(1, C)
-  
-  # Obtain the variances sp.i as a function of the c.i
-  sg[j,] <- rep(0, C)
-  
-  for (i in interval) {
-    indexes <- which(km.res$cluster == i)
-    (l.i <- length(indexes))
-    
-    # repeated.c <- c[i + 1,]
-    repeated.c <- do.call("rbind", replicate(N, c[i,], simplify = FALSE))
-    expxc[j,i] <- euc.dist(dd.num.s, repeated.c)
-    repeated.c <- do.call("rbind", replicate(l.i, c[i,], simplify = FALSE))
-    
-    tail(dd.num.s[indexes,])
-    tail(repeated.c)
-    
-    (sg[j, i] <- euc.dist(dd.num.s[indexes,], repeated.c)/l.i)
-    if (sg[j, i] == 0) sg[j, i] <- 1
-  }
-}
+summary(preds)
+
+show.test.neat <- show.test[-is.na(preds),]
+preds.neat <- preds[-is.na(preds),]
+
+(conftb <- table(show.test.neat, preds.neat > 0.6))
+sum(diag(conftb))/sum(conftb)
+
+## And now the normalized error of this prediction
+# N.test <- nrow(show.test.neat)
+# (errorsTest <- sqrt(sum((show.test.neat - preds.neat)^2)/((N.test-1)*var(show.test.neat))))
+
+## The results are poorer, with an advantage for the former method (using clustering)
+# 1-errorsTest^2
+
